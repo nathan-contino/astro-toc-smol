aba9829e0f5f719f7493623779332f132848a953
Nathan Contino <nathan.contino@fusionauth.io>
2026-09-24 10:41:42 -0400

    Fix spacing

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ function buildTocHtml(headings, minDepth, maxDepth) {
     if (!levelItems.length) return '';
 
     const isTop = depth === minDepth;
-    const classes = isTop ? 'space-y-3 pt-4' : 'space-y-2 ml-4 pt-2';
+    const classes = isTop ? 'space-y-2 pt-4' : 'space-y-2 ml-4 pt-2';
     const idAttr = isTop ? ' id="toc-list"' : '';
     let html = `<ul${idAttr} class="${classes}" data-widget="scroll-spy">`;
 
